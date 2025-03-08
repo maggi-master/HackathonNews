5a5d97472c80b4b9d54902e969d5b3b2c57db47e
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-08 11:57:18 +0100

    Laget overordnet mappe for nyheter api. Laget fil for RSS og klassen artikkel. Begynt på klassestruktur

---
+++ news_api/article.py
@@ -0,0 +1,7 @@
+class Article:
+    def __init__(self, source, link, title, description, pubDate) -> None:
+        self.source = source
+        self.link = link
+        self.title = title
+        self.description = description
+        self.pubDate = pubDate
---
+++ news_api/rss.py
@@ -0,0 +1,6 @@
+class RSS:
+    def __init__(self) -> None:
+        pass
+
+    def get_articles(self, feed:str, n_articles:int):
+        pass
