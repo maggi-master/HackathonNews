aaaa326a86335dd8c4ba51149802c0714231bf08
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-08 12:53:38 +0100

    Ferdigstile og dokumenterte funksjon som henter artikler via feed: get_articles()

--- news_api/rss.py
+++ news_api/rss.py
@@ -1,6 +1,13 @@
+import feedparser as fp
+from article import Article
+
 class RSS:
     def __init__(self) -> None:
         pass
 
-    def get_articles(self, feed:str, n_articles:int):
-        pass
+    def get_articles(self, source_name:str, feed:str)->list[Article]:
+        parsed_feed = fp.parse(feed)
+        articles = [Article(source_name, entry.title, entry.description, entry.published, entry.published_parsed, entry.link) for entry in parsed_feed.entries]
+        return articles
+
+
