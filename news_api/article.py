7d400a09a0b2c21a802f885a6ffae9c04700da59
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-16 11:28:19 +0100

    Restructured article class

--- news_api/article.py
+++ news_api/article.py
@@ -1,8 +1,8 @@
 class Article:
-    def __init__(self, source:str, title:str, description:str, pubDate:str, pubDate_parsed:str, link:str) -> None:
-        self.source = source
+    def __init__(self, source_name:str, title:str, description:str, link:str, pubDate:str=None, pubDate_parsed:str=None) -> None:
+        self.source = source_name
         self.title = title
         self.description = description
+        self.link = link
         self.pubDate = pubDate
         self.pubDate_parsed = pubDate_parsed
-        self.link = link
