0f17b556a44599e57278c063aebdd9ea64c08b75
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-16 11:32:35 +0100

    Reworked parse_feed function to avoid attribute error when pubDate didn't exist. Finished documentation. Added simple test

--- news_api/rss.py
+++ news_api/rss.py
@@ -1,13 +1,28 @@
 import feedparser as fp
+from rss_feeds import RSS_FEEDS
 from article import Article
 
 class RSS:
     def __init__(self) -> None:
-        pass
+        self._articles = []
+        for source_name, feed in RSS_FEEDS.items():
+            self._parse_feed(source_name, feed)
 
-    def get_articles(self, source_name:str, feed:str)->list[Article]:
+    def _parse_feed(self, source_name:str, feed:str) -> None:
+        """Parses single feed and appends to articles list"""
         parsed_feed = fp.parse(feed)
-        articles = [Article(source_name, entry.title, entry.description, entry.published, entry.published_parsed, entry.link) for entry in parsed_feed.entries]
-        return articles
+        for entry in parsed_feed.entries:
+            if hasattr(entry, "published"):
+                new_article = Article(source_name, entry.title, entry.description, entry.link, entry.published, entry.published_parsed)
+            else:
+                new_article = Article(source_name, entry.title, entry.description, entry.link)                
+            self._articles.append(new_article)
+        
+    def get_articles(self) -> list[Article]:
+        """Returns articles list"""
+        return self._articles
 
-
+if __name__ == "__main__":
+    rss = RSS()
+    for article in rss.get_articles():
+        print(f"{article.source}: {article.title} ({article.link})")
