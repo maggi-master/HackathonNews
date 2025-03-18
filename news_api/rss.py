5db5654af1bb1bd1080a2c79fe98e5a24d8a699c
Magnus <82754177+maggi-master@users.noreply.github.com>
2025-03-18 13:03:18 +0100

    Cleaned up rss.py file

--- news_api/rss.py
+++ news_api/rss.py
@@ -1,28 +1,21 @@
 import feedparser as fp
-from rss_feeds import RSS_FEEDS
-from article import Article
+from .rss_feeds import RSS_FEEDS
+from .Article import Article
 
 class RSS:
     def __init__(self) -> None:
-        self._articles = []
-        for source_name, feed in RSS_FEEDS.items():
-            self._parse_feed(source_name, feed)
+        self.articles = []
+        self.parse_feeds()
 
-    def _parse_feed(self, source_name:str, feed:str) -> None:
+    def parse_feed(self, source_name:str, feed:str) -> None:
         """Parses single feed and appends to articles list"""
         parsed_feed = fp.parse(feed)
         for entry in parsed_feed.entries:
-            if hasattr(entry, "published"):
-                new_article = Article(source_name, entry.title, entry.description, entry.link, entry.published, entry.published_parsed)
-            else:
-                new_article = Article(source_name, entry.title, entry.description, entry.link)                
-            self._articles.append(new_article)
-        
-    def get_articles(self) -> list[Article]:
-        """Returns articles list"""
-        return self._articles
-
-if __name__ == "__main__":
-    rss = RSS()
-    for article in rss.get_articles():
-        print(f"{article.source}: {article.title} ({article.link})")
+            article = Article(entry)
+            article.update({"source": source_name})
+            self.articles.append(article)
+    
+    def parse_feeds(self) -> None:
+        """Parses alle the feeds from RSS_FEEDS variable using the parse_feed function"""
+        for source_name, feed in RSS_FEEDS.items():
+            self.parse_feed(source_name, feed)
