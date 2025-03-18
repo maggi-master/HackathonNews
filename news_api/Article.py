a6a15e82fc8cd7bde30ebb2404b0713a7d349168
Magnus <82754177+maggi-master@users.noreply.github.com>
2025-03-18 11:29:17 +0100

    Renamed Article file

---
+++ news_api/Article.py
@@ -0,0 +1,2 @@
+class Article(dict):
+    pass
--- news_api/article.py
+++
@@ -1,8 +0,0 @@
-class Article:
-    def __init__(self, source_name:str, title:str, description:str, link:str, pubDate:str=None, pubDate_parsed:str=None) -> None:
-        self.source = source_name
-        self.title = title
-        self.description = description
-        self.link = link
-        self.pubDate = pubDate
-        self.pubDate_parsed = pubDate_parsed
