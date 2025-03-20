b259029847d0938a01958af105daa080023401d4
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-20 22:18:24 +0100

    Added new code to test error logging and web scraping

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
 import news_api
+import logging as log
 
 # Test for the rss module
+log.basicConfig(level=log.ERROR)
+
 api = news_api.RSS()
-api.articles[0].scrape_content()
-api.articles[0].print_article()
+articles = api.get_articles()
+print(articles[3].update_content())
+articles[3].print_article()
