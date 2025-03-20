113c1f365809b99ea710295504a632dadd87f052
Sebastian Hannevold <sebhan06@gmail.com>
2025-03-20 22:17:49 +0100

    Added protection IV and other functions for interacting with class: - get_articles() - print_articles()

--- news_api/rss.py
+++ news_api/rss.py
@@ -4,18 +4,27 @@ from .Article import Article
 
 class RSS:
     def __init__(self) -> None:
-        self.articles = []
-        self.parse_feeds()
+        self._articles = []
+        self._parse_feeds()
 
-    def parse_feed(self, source_name:str, feed:str) -> None:
+    def _parse_feed(self, source_name:str, feed:str) -> None:
         """Parses single feed and appends to articles list"""
         parsed_feed = fp.parse(feed)
         for entry in parsed_feed.entries:
             article = Article(entry)
             article.update({"source": source_name})
-            self.articles.append(article)
+            self._articles.append(article)
     
-    def parse_feeds(self) -> None:
+    def _parse_feeds(self) -> None:
         """Parses alle the feeds from RSS_FEEDS variable using the parse_feed function"""
         for source_name, feed in RSS_FEEDS.items():
-            self.parse_feed(source_name, feed)
+            self._parse_feed(source_name, feed)
+
+    def get_articles(self) -> list[Article]:
+        """Returns articles list"""
+        return self._articles
+
+    def print_articles(self) -> None:
+        """Prints out each article"""
+        for article in self._articles:
+            article.print_article()
