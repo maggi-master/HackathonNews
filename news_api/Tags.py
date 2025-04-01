11716f2f386053f35205da1b9884acbc4d6ded31
Magnus <82754177+maggi-master@users.noreply.github.com>
2025-04-01 22:37:20 +0200

    Added an embedding function for the tags

--- news_api/Tags.py
+++ news_api/Tags.py
@@ -4,3 +4,10 @@ import openai
 class Tags:
     def __init__(self):
         self._tags:list[Tag]
+
+    def embedd_tags(self, model="text-embedding-3-small"):
+        """Assigns a vector to all the tags using openai embedding"""
+        tags = [tag.tag for tag in self._tags]
+        embeddings = openai.embeddings.create(input = tags, model=model).data
+        for embedding, article in zip(embeddings, self._tags):
+            article.vector = np.array(embedding.embedding)
