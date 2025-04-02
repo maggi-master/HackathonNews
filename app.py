ed3e2fbbe6ad3d793809419540244beb40de46f4
Sebastian Hannevold <sebhan06@gmail.com>
2025-04-02 19:05:42 +0200

    PLZ WORK

--- app.py
+++ app.py
@@ -85,4 +85,5 @@ def save_data():
         return 'False'
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port, debug=False)
