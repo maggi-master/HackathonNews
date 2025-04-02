bccdbe91eec0ac9885cf66e4b77f05a55e91d799
Sebastian Hannevold <sebhan06@gmail.com>
2025-04-02 21:06:29 +0200

    Modified html_parser to accompany for more rss_feeds

--- news_api/rss_feeds.py
+++
@@ -1,7 +0,0 @@
-#more can be found on https://loop24.no/loopsign/rss-feeds/
-
-RSS_FEEDS = {
-    "NRK Toppsaker": "https://www.nrk.no/toppsaker.rss",
-    "Dagbladet": "https://www.dagbladet.no/?lab_viewport=rss",
-    "VG Forsiden": "https://www.vg.no/rss/feed/forsiden/"
-}
