ec3c8dcefb762db4d045e98ccc1dce427276e265
Magnus <82754177+maggi-master@users.noreply.github.com>
2025-04-02 16:56:50 +0200

    Added init function to article class so that self.vector is declared properly

--- news_api/Article.py
+++ news_api/Article.py
@@ -3,13 +3,16 @@ from .html_parser import HTMLParser
 import numpy as np
 
 class Article(dict):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vector:np.ndarray = None
+    
     def update_content(self):
         """Updates content by scraping from stored URL"""
         url = self.get("link", '')
         html_parser = HTMLParser(url)
         html_parser.scrape_content()
         self["content"] = html_parser.get_content()
-        self.vector:np.ndarray = None
 
     def __str__(self):
         """Returns the article values in a beautiful way"""
