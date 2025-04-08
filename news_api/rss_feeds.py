ac767a86fd3feecfc445b458068826334cc0629d
Magnus <82754177+maggi-master@users.noreply.github.com>
2025-04-08 10:19:05 +0200

    Added more sites to fetch news from

--- news_api/rss_feeds.py
+++ news_api/rss_feeds.py
@@ -3,5 +3,7 @@
 RSS_FEEDS = {
     "NRK Toppsaker": "https://www.nrk.no/toppsaker.rss",
     "Dagbladet": "https://www.dagbladet.no/?lab_viewport=rss",
-    "VG Forsiden": "https://www.vg.no/rss/feed/forsiden/"
+    "VG Utenriks": "https://www.vg.no/rss/feed/?categories=1070",
+    "Reuters": "https://news.google.com/rss/search?q=site%3Areuters.com&hl=en-US&gl=US&ceid=US%3Aen",
+    "E24": "http://e24.no/rss2/"
 }
